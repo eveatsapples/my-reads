1b86f9f6e009b1b1217bab56cdfac2815870100f
Everett Penner <everettpenner@codebusiness.ca>
2019-02-01 16:08:59 -0600

    Updates

---
+++ precache-manifest.866928e60d1543c55ac5002b71f77a85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "ca74759a619a6021c9ea",
+    "url": "/my-reads/static/css/main.a951bdc3.chunk.css"
+  },
+  {
+    "revision": "fcbe5eb6849fb1e3dd62",
+    "url": "/my-reads/static/js/1.fcbe5eb6.chunk.js"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "ca74759a619a6021c9ea",
+    "url": "/my-reads/static/js/main.ca74759a.chunk.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "5128eaf16911369d4822cec990cb96b9",
+    "url": "/my-reads/static/media/baseline-hourglass_empty-24px.5128eaf1.svg"
+  },
+  {
+    "revision": "7b539e135651a5069324c2515a190b29",
+    "url": "/my-reads/static/media/baseline-favorite_border-24px.7b539e13.svg"
+  },
+  {
+    "revision": "f88f4a8e0cdee4521743c5d644d89293",
+    "url": "/my-reads/static/media/baseline-done-24px.f88f4a8e.svg"
+  },
+  {
+    "revision": "de1bf375a3c22a1fc8102ea40fb916db",
+    "url": "/my-reads/index.html"
+  }
+];
